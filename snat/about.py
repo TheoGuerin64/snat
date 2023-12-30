988ad0930aeead41491bcb33cdff099732d54316
TheoGuerin64 <tguerin@student.42angouleme.fr>
2023-12-30 16:10:19 +0100

    Add documentation for the about dialog and __main__

--- snat/about.py
+++ snat/about.py
@@ -10,18 +10,25 @@ LICENSE_URL = "https://github.com/TheoGuerin64/snat/blob/main/LICENSE"
 
 
 class AboutDialog(QtWidgets.QDialog):
+    """Dialog that display information about the application.
+
+    Args:
+        parent (QtWidgets.QWidget): Parent widget
+    """
     def __init__(self, parent: QtWidgets.QWidget) -> None:
         super().__init__(parent)
         self.init_ui()
 
     def init_ui(self) -> None:
+        """Initialize the user interface."""
         self.setWindowTitle("About")
         self.setWindowIcon(QtGui.QIcon("asset:icon.ico"))
 
         layout = QtWidgets.QVBoxLayout(self)
         self.setLayout(layout)
 
-        self.init_top(layout)
+        top = self.init_top()
+        layout.addLayout(top)
 
         button_box = QtWidgets.QDialogButtonBox(QtWidgets.QDialogButtonBox.StandardButton.Ok, self)
         button_box.accepted.connect(self.accept)
@@ -29,23 +36,34 @@ class AboutDialog(QtWidgets.QDialog):
 
         self.setFixedSize(self.sizeHint())
 
-    def init_top(self, layout: QtWidgets.QVBoxLayout) -> None:
-        top_layout = QtWidgets.QHBoxLayout()
-        layout.addLayout(top_layout)
+    def init_top(self) -> QtWidgets.QHBoxLayout:
+        """Initialize the top part of the user interface.
 
+        Returns:
+            QtWidgets.QHBoxLayout: The top layout
+        """
+        top_layout = QtWidgets.QHBoxLayout()
         label = QtWidgets.QLabel(self)
         label.setAlignment(QtCore.Qt.AlignmentFlag.AlignTop)
         label.setPixmap(QtGui.QPixmap("asset:icon.ico").scaledToHeight(64))
         top_layout.addWidget(label)
 
-        form = QtWidgets.QFormLayout()
-        self.add_rows(form)
+        form = self.init_form()
         top_layout.addLayout(form)
 
-    def add_rows(self, form: QtWidgets.QFormLayout) -> None:
+        return top_layout
+
+    def init_form(self) -> QtWidgets.QFormLayout:
+        """Initialize the form layout.
+
+        Returns:
+            QtWidgets.QFormLayout: The form layout
+        """
+        form = QtWidgets.QFormLayout()
         version_url = f"{CHANGELOG_URL}#{__version__.replace('.', '')}"
         form.addRow("Version", LinkLabel(f"{__version__} (<a href={version_url}>changelog</a>)", self))
         form.addRow("Author", LinkLabel(f"<a href={GITHUB_PROFIL_URL}>Théo Guérin</a>", self))
         form.addRow("License", LinkLabel(f"<a href={LICENSE_URL}>GPLv3</a>", self))
         form.addRow("Github", LinkLabel(f"<a href={GITHUB_REPOSITORY_URL}>Snat</a>", self))
         form.addRow("PyQt6", QtWidgets.QLabel("6.0.3"))
+        return form
